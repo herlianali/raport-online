ef0646b7136272018fe94545d07a92ab61c46f06
nasrudiniqrok <nasrudinrudin17@gmail.com>
2022-12-08 22:07:10 +0700

    tampilan dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,30 +5,72 @@
 <div class="main-content">
     <section class="section">
     <div class="section-header">
-        <h1>Blank Page</h1>
+        <h1>Dashboard</h1>
     </div>
 
-    <div class="section-body">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('Dashboard') }}</div>
-
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-
-                            {{ __('You are logged in!') }}
-                        </div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                    <a href="{{url('/home')}}" class="color-white"><i class="fas fa-user-graduate"></i></a>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Guru</h4>
+                    </div>
+                    <div class="card-body">
+                        10
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-success">
+                    <a href="{{url('/home')}}" class="color-white"><i class="fas fa-users"></i></a>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Siswa</h4>
+                    </div>
+                    <div class="card-body">
+                        40
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                    <a href="{{url('/home')}}" class="color-white"><i class="fas fa-school"></i></a>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Kelas</h4>
+                    </div>
+                    <div class="card-body">
+                        6
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <div class="card card-statistic-1">
+                <div class="card-icon bg-info">
+                    <a href="{{url('/home')}}" class="color-white"><i class="fas fa-book"></i></a>
+                </div>
+                <div class="card-wrap">
+                    <div class="card-header">
+                        <h4>Total Mata Pelajaran</h4>
+                    </div>
+                    <div class="card-body">
+                        12
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
     </section>
 </div>
